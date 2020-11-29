4717ad3e77fbf2a557741fd86590bd136ae6ffca
ts-miller <tylerstephen11@gmail.com>
2020-11-29 11:03:03 -0500

    commit tree 6816e59d74dca0d308be4dcc9efcad6700a2e16e

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1
+
+const mod = 10 % 6
+
+const max = Math.max(1, 18, 3, 10, 20)
